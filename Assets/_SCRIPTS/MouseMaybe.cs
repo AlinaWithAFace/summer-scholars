e7c6ab968e7d1cd1bf15386369f7d982efae4c8f
Alina Christenbury <alina.christenbury@gmail.com>
2018-07-03 13:34:18 -0400

    fiddled with scripts

---
+++ Assets/_SCRIPTS/MouseMaybe.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using Leap;
+using Leap.Unity;
+
+public class MouseMaybe : MonoBehaviour
+{
+    [DllImport("user32.dll")]
+    static extern bool SetCursorPos(int X, int Y);
+
+    public int XPos = 30, YPos = 1000;
+
+    public int ScreenWidth = Screen.width;
+    public int ScreenHeight = Screen.height;
+    public HandModelBase PointerHand;
+
+    Vector3 _palmPosition;
+    Vector3 _palmDirection;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Screen Width : " + ScreenWidth);
+        Debug.Log("Screen Height : " + ScreenHeight);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _palmPosition = PointerHand.GetLeapHand().PalmPosition.ToVector3();
+        _palmDirection = PointerHand.GetLeapHand().Direction.ToVector3();
+
+        Debug.DrawRay(_palmPosition, _palmDirection);
+        //SetCursorPos(XPos, YPos); //Call this when you want to set the mouse position
+    }
+}
---
+++ Assets/_SCRIPTS/OldJavaPorts/ActionFlag.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//
+///// <summary>
+///// The various actions that can be performed, along with a flag to tell whether or not it should be happening right now
+///// </summary>
+//public static class ActionFlag
+//{
+//    public enum Flags
+//    {
+//        MoveForward,
+//        MoveBackward,
+//        MoveLeft,
+//        MoveRight,
+//        Jump,
+//        Crouch,
+//        RightBioticGrasp,
+//        LeftBioticGrasp,
+//        BioticOrb,
+//        Coalescence,
+//        Fade,
+//        Melee,
+//        Wave,
+//    }
+//
+//    private static int flagCount = Enum.GetNames(typeof(Flags)).Length;
+//    public static bool[] flagValues = new bool[flagCount];
+//}
