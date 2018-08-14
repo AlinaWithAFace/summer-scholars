d206c434e88828c19ce0ba85d45c0e8b9ce241c5
Alina Christenbury <alina.christenbury@gmail.com>
2018-08-14 10:41:59 -0400

    added tiltbrush tools, a custom skybox

---
+++ Assets/ForceForward.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceForward : MonoBehaviour
+{
+
+	public float thrust;
+	public GameObject ThingToForceForward;
+	public bool debugTrue = true;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.CompareTag("Player") || debugTrue)
+		{
+			//ThingToForceForward.transform.forward;
+		}
+	}
+}
