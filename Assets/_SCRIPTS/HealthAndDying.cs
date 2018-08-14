ab5d95651273ceaf225b4486b5611f986c163033
Alina Christenbury <alina.christenbury@gmail.com>
2018-08-14 11:21:36 -0400

    added ship and control panel whiteboxes

--- Assets/_SCRIPTS/HealthAndDying.cs
+++ Assets/_SCRIPTS/HealthAndDying.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class HealthAndDying : MonoBehaviour
 {
     public float MaxHealth = 3;
---
+++ Assets/_SCRIPTS/ShipControls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[]
+public class ShipControls : MonoBehaviour
+{
+    private int selected = 0;
+
+    public string[] ButtonSelect = new string[]
+    {
+        "FORWARD", "BACKWARD", "LEFT", "RIGHT"
+    };
+
+    
+    public float thrust;
+    public Transform ThingToForceForward;
+    public bool debugTrue = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        selected = EditorGUILayout.Popup("Label", selected, ButtonSelect);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player") || debugTrue)
+        {
+           // ThingToForceForward.forward;
+        }
+    }
+    
+}
